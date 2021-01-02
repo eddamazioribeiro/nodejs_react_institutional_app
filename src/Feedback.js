2fa75defead6d2ffd8c382ab598774ac309910fb
Eduardo Ribeiro <ed.damazioribeiro@gmail.com>
2021-01-02 15:59:44 -0300

    Created form on Feedback component

--- src/Feedback.js
+++ src/Feedback.js
@@ -14,16 +14,75 @@ const Feedback = () => {
   // destrutcturing state vars
   const {
     name,
-    emailname,
-    messagename,
-    phonename,
-    uploadedFilesname,
-    buttonTextname,
-    uploadPhotosButtonTextname
+    email,
+    message,
+    phone,
+    uploadedFiles,
+    buttonText,
+    uploadPhotosButtonText
   } = values;
 
+  const handleChange = () => {
+    console.log('handle change');
+  };
+
+  const handleSubmit = () => {
+    console.log('handle');
+  };
+
+  const feedbackForm = () => {
+    return(
+      <React.Fragment>
+        <form onSubmit={handleSubmit}>
+          <div className="form-group">
+            <label className="text-muted">Description</label>
+            <textarea
+              type="text"
+              className="form-control"
+              required
+              onChange={handleChange('message')}
+              value={message}></textarea>
+          </div>
+          <div className='form-group'>
+            <label className="text-muted">Name</label>
+            <input className="form-control"
+              type="text"
+              required
+              onChange={handleChange('name')}
+              value={name}>
+            </input>
+          </div>
+          <div className='form-group'>
+            <label className="text-muted">Email</label>
+            <input className="form-control"
+              type="text"
+              required
+              onChange={handleChange('email')}
+              value={email}>
+            </input>
+          </div>
+          <div className='form-group'>
+            <label className="text-muted">Phone</label>
+            <input className="form-control"
+              type="number"
+              required
+              onChange={handleChange('phone')}
+              value={phone}>
+            </input>
+          </div>
+          <button className="col-12 mt-2 btn btn-primary btn-block">{buttonText}</button>                  
+        </form>
+      </React.Fragment>
+
+    );
+  };
+
   return(
-    <p>Feedback Page</p>
+    <div className="p-5">
+      <h3>Feedback</h3>
+      <hr/>
+      {feedbackForm()}
+    </div>
   );
 };
 
